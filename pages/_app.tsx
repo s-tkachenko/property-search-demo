9ee74e6c5c2b4aebf1eae4357a1c41f0d43b70f7
Semyon Tkachenko <s.tkachenko.dev@gmail.com>
2021-01-31 05:11:57 +0100

    Add Layout component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,14 @@ import '../styles/globals.css';
 
 import { AppProps } from 'next/app';
 
+import Layout from '../components/Layout/Layout';
+
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
 export default App;
