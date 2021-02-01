87275bdd6a4100e3c1ebdfc6a0bf7b31788bb881
Semyon Tkachenko <s.tkachenko.dev@gmail.com>
2021-02-01 01:17:48 +0100

    Add footer

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+import styles from './Footer.module.css';
+
+export default function Footer() {
+  return (
+    <footer className={styles.container}>
+      <div>© 2020 Property and Homes Search</div>
+    </footer>
+  );
+}
