4bc4516390c96b33ec6616634d6c7033714c5fc7
Semyon Tkachenko <s.tkachenko@pixwood.biz>
2021-02-03 02:12:33 +0100

    Update style of favorites button

--- components/ButtonFavorites/ButtonFavorites.tsx
+++ components/ButtonFavorites/ButtonFavorites.tsx
@@ -3,7 +3,6 @@ import styles from './ButtonFavorites.module.css';
 export default function ButtonFavorites() {
   return (
     <a href="#" className={styles.container}>
-      <div className={styles.icon} />
       Favorites
     </a>
   );
