0567cdf74f2c9587f4d4805136653ec5ed82e227
Semyon Tkachenko <s.tkachenko@pixwood.biz>
2021-02-11 00:32:41 +0100

    Update footer text

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import styles from './Footer.module.css';
 export default function Footer() {
   return (
     <footer className={styles.container}>
-      <div>© 2021 Property and Homes Search</div>
+      <div>© 2021 Search properties for sale in the UK</div>
     </footer>
   );
 }
