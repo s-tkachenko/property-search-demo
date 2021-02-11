b779e5a68c29410427e2c12a4f80c387a4f442dc
Semyon Tkachenko <s.tkachenko@pixwood.biz>
2021-02-11 01:27:30 +0100

    Create separate apartments list component

---
+++ components/ApartmentsList/ApartmentsList.tsx
@@ -0,0 +1,53 @@
+import Link from 'next/link';
+
+import MSG from '../../constants/messages';
+import { CLIENT } from '../../constants/routes';
+import { Apartment } from '../../types/apartment';
+import ApartmentCard from '../ApartmentCard/ApartmentCard';
+import ContentGrid from '../ContentGrid/ContentGrid';
+import ErrorMessage from '../ErrorMessage/ErrorMessage';
+import LoadingSpinner from '../LoadingSpinner/LoadingSpinner';
+import Pagination from '../Pagination/Pagination';
+
+type Props = {
+  error: Error;
+  data: {
+    apartments: Apartment[];
+    page: number;
+    totalPages: number;
+  };
+  location: string;
+};
+
+export default function ApartmentsList({ error, data, location }: Props) {
+  const isNoResults = !Array.isArray(data?.apartments) || data.apartments.length === 0;
+
+  if (error) {
+    return <ErrorMessage message={MSG.ERROR_FAIL_TO_LOAD} />;
+  }
+  if (!data) {
+    return <LoadingSpinner />;
+  }
+  if (isNoResults) {
+    return <ErrorMessage message={MSG.ERROR_NO_RESULTS(location)} />;
+  }
+
+  return (
+    <>
+      <ContentGrid>
+        {data.apartments.map((apartment: Apartment) => (
+          <Link href={CLIENT.APARTMENT_BY_ID(apartment.id)} key={apartment.id}>
+            <a>
+              <ApartmentCard apartment={apartment} />
+            </a>
+          </Link>
+        ))}
+      </ContentGrid>
+      <Pagination
+        currentPage={data.page}
+        totalPages={data.totalPages}
+        baseUrl={CLIENT.FIND_APARTMENTS_BY_QUERY(location)}
+      />
+    </>
+  );
+}
--- pages/for-sale/[location].tsx
+++ pages/for-sale/[location].tsx
@@ -1,24 +1,17 @@
-import Link from 'next/link';
 import { useRouter } from 'next/router';
 import useSWR from 'swr';
 
-import ApartmentCard from '../../components/ApartmentCard/ApartmentCard';
-import ContentGrid from '../../components/ContentGrid/ContentGrid';
-import ErrorMessage from '../../components/ErrorMessage/ErrorMessage';
-import LoadingSpinner from '../../components/LoadingSpinner/LoadingSpinner';
+import ApartmentsList from '../../components/ApartmentsList/ApartmentsList';
 import PageHead from '../../components/PageHead/PageHead';
-import Pagination from '../../components/Pagination/Pagination';
 import SearchBar from '../../components/SearchBar/SearchBar';
 import { DEFAULT } from '../../constants/common';
-import MSG from '../../constants/messages';
-import { API, CLIENT } from '../../constants/routes';
+import { API } from '../../constants/routes';
 import TITLE from '../../constants/titles';
 import {
   fetcher,
   getRouterParamIntValue,
   getRouterParamStringValue
 } from '../../services/api/helpers';
-import { Apartment } from '../../types/apartment';
 
 export default function ForSale() {
   const router = useRouter();
@@ -26,37 +19,12 @@ export default function ForSale() {
   const pageIndex = getRouterParamIntValue(router?.query?.page) || DEFAULT.PAGE_INDEX;
 
   const { data, error } = useSWR(API.LOCATION_SEARCH(location, pageIndex), fetcher);
-  const isNoResults = !Array.isArray(data?.apartments) || data.apartments.length === 0;
-
-  const renderContent = () => {
-    if (error) return <ErrorMessage message={MSG.ERROR_FAIL_TO_LOAD} />;
-    if (!data) return <LoadingSpinner />;
-    if (isNoResults) return <ErrorMessage message={MSG.ERROR_NO_RESULTS(location)} />;
-    return (
-      <>
-        <ContentGrid>
-          {data.apartments.map((apartment: Apartment) => (
-            <Link href={CLIENT.APARTMENT_BY_ID(apartment.id)} key={apartment.id}>
-              <a>
-                <ApartmentCard apartment={apartment} />
-              </a>
-            </Link>
-          ))}
-        </ContentGrid>
-        <Pagination
-          currentPage={data.page}
-          totalPages={data.totalPages}
-          baseUrl={CLIENT.FIND_APARTMENTS_BY_QUERY(location)}
-        />
-      </>
-    );
-  };
 
   return (
     <>
       <PageHead title={TITLE.SEARCH_RESULTS(location)} />
       <SearchBar value={location} />
-      {renderContent()}
+      <ApartmentsList error={error} data={data} location={location} />
     </>
   );
 }
