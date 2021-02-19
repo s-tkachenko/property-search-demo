43005e5e747dcd5495075dfc1e3eabceb11d6df8
Semyon Tkachenko <s.tkachenko@pixwood.biz>
2021-02-19 20:20:32 +0100

    build: add eslint-plugin for jest and testing-library

--- components/Footer/Footer.test.tsx
+++ components/Footer/Footer.test.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import Footer from './Footer';
-
 import { render, screen } from '@testing-library/react';
+import React from 'react';
+
+import Footer from './Footer';
 
 it('renders with a copyright text', () => {
   const testMessage = '© 2021';
